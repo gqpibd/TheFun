0d3a654fc3daa1385f5dc026f7752e90b400a901
seungji <seungji@aaa-PC>
2018-12-06 16:40:28 +0900

    Master랑 머지하고 MVC구조에 Basket바뀐것도 전부 반영

--- TheFun/src/donzo/thefun/controller/BasketController.java
+++ TheFun/src/donzo/thefun/controller/BasketController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import donzo.thefun.model.BasketDto;
+import donzo.thefun.model.BuyDto;
 import donzo.thefun.service.BasketService;
 
 @Controller
@@ -20,32 +21,6 @@ public class BasketController {
 	 
 	@Autowired
 	BasketService basketService;
-	
-
-	// 장바구니 창으로
-	@RequestMapping(value="myBasket.do", method= {RequestMethod.GET, RequestMethod.POST})
-	public String myBasket(String id, Model model) throws Exception {
-		logger.info("BuyController basket들어옴 " + new Date());
-		logger.info("아이디 = " + id);
-		// 내 장바구니 목록(view) 찾아오기(SEQ, ID, PROJECTSEQ, OPTIONSEQ, COUNT, REGDATE, PTITLE, OTITLE, OCONTENT, STATUS, PRICE)
-		List<BasketDto> myBasketList = basketService.selectMyBasket(id);
-		logger.info("찾아온 장바구니 목록 개수 = " + myBasketList.size());  
-		for (int i = 0; i < myBasketList.size(); i++) {
-			logger.info("찾아온 장바구니 = " + myBasketList.toString());
-		}
-		model.addAttribute("myBasket", myBasketList);
-		
-		return "basket.tiles";
-	}
-	
-	
-	
-	@RequestMapping(value="testbasket.do", method= {RequestMethod.GET, RequestMethod.POST}) 
-	public void testbasket() {
-		logger.info("testbasket" + new Date());
-		logger.info("●●●●●●잘들어왔슈~~~~~~~~~~~~~~~~~~~");
-		
-	}
 	
 	
 	//장바구니 넣기 addBasket.do
@@ -61,4 +36,47 @@ public class BasketController {
 		//장바구니 insert
 		 
 	}
+	
+	// 장바구니 창으로
+	@RequestMapping(value="myBasket.do", method= {RequestMethod.GET, RequestMethod.POST})
+	public String myBasket(String id, Model model) throws Exception {
+		logger.info("BuyController basket 들어옴 " + new Date());
+		logger.info("아이디 = " + id);
+		// 내 장바구니 목록(view) 찾아오기(SEQ, ID, PROJECTSEQ, OPTIONSEQ, COUNT, REGDATE, PTITLE, OTITLE, OCONTENT, STATUS, PRICE)
+		List<BasketDto> myBasketList = basketService.selectMyBasket(id);
+		logger.info("찾아온 장바구니 목록 개수 = " + myBasketList.size());
+		for (int i = 0; i < myBasketList.size(); i++) {
+			logger.info("찾아온 장바구니 = " + myBasketList.toString());
+		}
+		model.addAttribute("myBasket", myBasketList);
+		
+		return "basket.tiles";
+	}
+	
+	// 장바구니 삭제
+	@RequestMapping(value="deleteBasket.do", method= {RequestMethod.GET, RequestMethod.POST})
+	public String deleteBasket(int seq, String id) throws Exception{
+		logger.info("BuyController deleteBasket 들어옴 " + new Date());
+		logger.info("삭제할 장바구니의 seq값 = " + seq);
+		logger.info("삭제할 장바구니의 주인 id = " + id);
+		
+		// 장바구니 DB에서 해당 리워드 삭제
+		int success = basketService.deleteBasket(seq);
+		if(success > 0) {
+			logger.info("장바구니 삭제에 성공했습니다");
+		}else {
+			logger.info("장바구니 삭제에 실패했습니다");
+		}
+		
+		return "redirect:myBasket.do?id="+id;
+	}
+	
+	// 장바구니 업데이트
+	@RequestMapping(value="updateBasket.do", method= {RequestMethod.GET, RequestMethod.POST})
+	public String updateBasket(String id) throws Exception{
+		logger.info("BuyController deleteBasket 들어옴 " + new Date());
+		
+		return "redirect:myBasket.do?id="+id;
+	}
+	
 }
